2607b6e600888bdee86c01c8daf4f70908adc013
kondasg <gabor@kondas.hu>
2021-09-06 15:14:39 +0200

    remove file

---
+++ src/main/java/training360/guinessapp/Recorder.java
@@ -0,0 +1,28 @@
+package training360.guinessapp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "recorder")
+public class Recorder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    public Recorder(String name, LocalDate dateOfBirth) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
+}
